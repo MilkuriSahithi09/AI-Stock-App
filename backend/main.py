bd14f5042797b48d9eea46fc422a24e63b900fb6
sahithimilkuri <sahithimilkuri5@gmail.com>
2026-05-04 14:29:13 -0400

    final fix structure

---
+++ backend/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+from datetime import datetime, timedelta
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+API_KEY = "4234ada308d3402884a1f83608eec617"
+
+@app.get("/")
+def home():
+    return {"status": "working"}
+
+@app.get("/search")
+def search_news(query: str):
+
+    from_date = (datetime.now() - timedelta(days=7)).strftime("%Y-%m-%d")
+
+    url = f"https://newsapi.org/v2/everything?q={query}&language=en&from={from_date}&sortBy=publishedAt&apiKey={API_KEY}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    articles = data.get("articles", [])[:10]
+
+    results = []
+
+    for article in articles:
+        results.append({
+            "title": article.get("title", ""),
+            "description": article.get("description", "") or "",
+            "source": article["source"]["name"],
+            "url": article["url"],
+            "sentiment": "Neutral"
+        })
+
+    return {"results": results}
